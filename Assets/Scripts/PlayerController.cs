b15f77193f18cbe74f9fea80e7e233e71430daee
Mert Sen <mert.senn21@gmail.com>
2024-02-08 20:18:26 +0300

    HorizontalMoveReady

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+    public float moveSpeed = 1f;
+
+    bool facingRight = true;
+
+    void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove();
+        if(playerRB.velocity.x < 0 && facingRight)
+        {
+            FlipFace();
+        }
+        else if(playerRB.velocity.x > 0 && !facingRight)
+        {
+            FlipFace();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void HorizontalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+        playerAnimator.SetFloat("playerSpeed",Mathf.Abs(playerRB.velocity.x));
+    }
+
+    void FlipFace()
+    {
+        facingRight = !facingRight;
+        Vector3 tempLocalScale = transform.localScale;
+        tempLocalScale.x *= -1;
+        transform.localScale = tempLocalScale;
+    }
+}
